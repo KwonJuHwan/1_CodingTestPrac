51913db42116ac81bef2d00fd0d6cc9de50b193a
JuHwan Kwon <118177454+KwonJuHwan@users.noreply.github.com>
2023-04-19 21:36:32 +0900

    Add files via upload

---
+++ 82_1256.py
@@ -0,0 +1,30 @@
+# 사전찾기
+
+n, m, k = map(int, input().split())
+
+# k에 대한 이해
+# 몇번째 문자열을 표현해야 하는지 : k
+
+D = [[0 for _ in range(202)] for _ in range(202)]
+
+for i in range(0, 201):
+    for j in range(0, i + 1):
+        if j == 0 or j == i:
+            D[i][j] == 1
+        else:
+            D[i][j] = D[i - 1][j] + D[i - 1][j - 1]
+            if D[i][j] > 1000000000:
+                D[i][j] = 1000000001
+
+if D[n + m][m] < k:
+    print(-1)
+
+else:
+    while not (n == 0 and m == 0):
+        if D[n - 1 + m][m] >= k:
+            print('a', end='')
+            n -= 1
+        else:
+            print('z', end='')
+            k -= D[n - 1 + m][m]
+            m -= 1
