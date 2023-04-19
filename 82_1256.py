b3d252d585a3f0cd8565859d8d40ab39bbcee974
JuHwan Kwon <118177454+KwonJuHwan@users.noreply.github.com>
2023-04-19 21:43:28 +0900

    Add files via upload

--- 82_1256.py
+++ 82_1256.py
@@ -10,11 +10,12 @@ D = [[0 for _ in range(202)] for _ in range(202)]
 for i in range(0, 201):
     for j in range(0, i + 1):
         if j == 0 or j == i:
-            D[i][j] == 1
+            D[i][j] = 1
         else:
             D[i][j] = D[i - 1][j] + D[i - 1][j - 1]
             if D[i][j] > 1000000000:
                 D[i][j] = 1000000001
+
 
 if D[n + m][m] < k:
     print(-1)
